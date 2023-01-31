1455541cda1f096537ea01c50829b81414c7405f
daniel2005erm <iliaz1986kg1986@gmail.com>
2023-01-31 18:44:48 +0600

    dz4

---
+++ hero.py
@@ -0,0 +1,20 @@
+# class A:
+#     def __init__(self,name) -> None:
+#         self.name = name
+# class B:
+#     def __init__(self,age) -> None:
+#         self.age = age
+# class C:
+#     def a():
+#         pass
+# class D:
+#     def b():
+#         pass
+# class Dan(A,B,C,D):
+#     def __init__(self, name,age) -> None:
+#         A.__init__(self,name)
+#         B.__init__(self,age)
+#     def __str__(self) -> str:
+#         return f"{self.name}, {self.age}"
+# dan = Dan("Daniul",17)
+# print(dan)
